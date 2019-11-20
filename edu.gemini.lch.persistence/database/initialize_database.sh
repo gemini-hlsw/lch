177a57d59073095c65c4cf9baa0ece70338872d2
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2019-11-20 14:17:08 -0300

    Build using github actions
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- edu.gemini.lch.persistence/database/initialize_database.sh
+++ edu.gemini.lch.persistence/database/initialize_database.sh
@@ -7,6 +7,6 @@ database=$2
 
 echo "Initializing databases $database for user $datauser "
 
-find . -name "*.sql" -maxdepth 1 | env LC_ALL=C sort -n | xargs -I {} psql -f {} -d $database -U $datauser | grep "NOTICE\|ERROR"
+find . -name "*.sql" -maxdepth 1 | env LC_ALL=C sort -n | xargs -I {} psql -f {} -d $database -U $datauser -h 127.0.0.1 | grep "NOTICE\|ERROR"
 
 exit 0
