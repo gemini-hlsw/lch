01ddb118e0c18d18025964646f2386ddf6879adf
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2019-11-22 13:19:21 -0300

    Fail if the required env variables are missing
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- edu.gemini.lch.services/src/main/scala/edu/gemini/lch/services/impl/JSpOCCredentials.scala
+++ edu.gemini.lch.services/src/main/scala/edu/gemini/lch/services/impl/JSpOCCredentials.scala
@@ -6,10 +6,12 @@ import org.apache.commons.httpclient.UsernamePasswordCredentials
 import scala.collection.immutable.HashMap
 
 object JSpOCCredentials {
-  val gsUserName = Option(System.getenv("GS_USERNAME")).getOrElse("")
-  val gsPassword = Option(System.getenv("GS_PASSWORD")).getOrElse("")
-  val gnUserName = Option(System.getenv("GN_USERNAME")).getOrElse("")
-  val gnPassword = Option(System.getenv("GN_PASSWORD")).getOrElse("")
+  def unsafeGetEnv(key: String): String =
+    Option(System.getenv(key)).getOrElse(sys.error(s"Missing environment variable $key"))
+  val gsUserName = unsafeGetEnv("GS_USERNAME")
+  val gsPassword = unsafeGetEnv("GS_PASSWORD")
+  val gnUserName = unsafeGetEnv("GN_USERNAME")
+  val gnPassword = unsafeGetEnv("GN_PASSWORD")
 
   // The Gemini credentials by site.
   val CredentialsLookup = HashMap[Site, UsernamePasswordCredentials](
